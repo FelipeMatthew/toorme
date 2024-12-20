a46c636422db2f1b3e080b51d1d8921a9b5ee4a5
felipematthew <felipematthewnascimento@gmail.com>
2024-12-19 22:27:28 -0300

    feat: docker & docker compose

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'toorme',
+      entities: [__dirname + "/../**/*.entity.{js,ts}"],
+      synchronize: true, // todo: remove later
+    })
+    ,UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
